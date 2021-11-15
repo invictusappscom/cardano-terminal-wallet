8595de0b00724bdd9871e8c62a8297c3b3730f1b
Zarko Jevtic <zarej@svrljig.net>
2021-11-15 21:02:54 +0100

    fixed batch swap

--- use-cases/swap/batch-tx.sh
+++ use-cases/swap/batch-tx.sh
@@ -10,35 +10,47 @@ MY_ADDR=$(cat $DIR/payment.addr)
 CHANGE_ADDR=$MY_ADDR
 SCRIPT="$ROOT_DIR/plutus/AlwaysSucceeds.plutus"
 SCRIPT_ADDR=$(${CARDANO_CLI_PATH} address build --payment-script-file $SCRIPT $NETWORK)
+echo ""
 
+# Show wallet
+# . $ROOT_DIR/balance-addr.sh $MY_ADDR
+${CARDANO_CLI_PATH} query utxo $NETWORK --address $MY_ADDR
+read -p "Enter UTxO from above list (it will be used as collateral): " MY_UTXO
 
-echo "List of hashes"
+echo "List of hashes:"
+printf '%.s─' $(seq 1 $(tput cols))
 # zarko
 ADDR1=addr_test1qqp6z9xneafqp0ndrxgyjd3qecs4tf6ktqhvj2hyc5efg2gzq5lqcm6rfuv6yp0mtsgc2qxvwlwr938qk2nue0rqg6rqxewe52
 DATUM_HASH=$(${CARDANO_CLI_PATH} transaction hash-script-data --script-data-value '["'${ADDR1:0:64}'","'${ADDR1:64}'"]')
 echo "Data: $ADDR1 = "\'[\"${ADDR1:0:64}\",\"${ADDR1:64}\"]\'
 echo "Datum hash: $DATUM_HASH"
+printf '%.s─' $(seq 1 $(tput cols))
 # zarej
 ADDR2=addr_test1qzxvxzjkpz4lqcrqse8zz3zpmxlzxcnf90p9lksjae2fl0dtqdv4srf98l7l6km72a5jlwngmvcj6xz2yv33cf8j0jdsetflek
 DATUM_HASH=$(${CARDANO_CLI_PATH} transaction hash-script-data --script-data-value '["'${ADDR2:0:64}'","'${ADDR2:64}'"]')
 echo "Data: $ADDR2 = "\'[\"${ADDR2:0:64}\",\"${ADDR2:64}\"]\'
 echo "Datum hash: $DATUM_HASH"
+printf '%.s─' $(seq 1 $(tput cols))
 # zare2
 ADDR3=addr_test1qqvxdvjcrwzqskge258qwax59nmuvt0veflk8pn4mmx603zv5t38e79gj3lgmvfdm8ntr2yau5khprzyyhzvn3lx04ssxrx6mt
 DATUM_HASH=$(${CARDANO_CLI_PATH} transaction hash-script-data --script-data-value '["'${ADDR3:0:64}'","'${ADDR3:64}'"]')
 echo "Data: $ADDR3 = "\'[\"${ADDR3:0:64}\",\"${ADDR3:64}\"]\'
 echo "Datum hash: $DATUM_HASH"
+printf '%.s─' $(seq 1 $(tput cols))
 # Script
 DATUM_HASH=$(${CARDANO_CLI_PATH} transaction hash-script-data --script-data-value '["'${SCRIPT_ADDR:0:64}'","'${SCRIPT_ADDR:64}'"]')
-echo "Data: $SCRIPT_ADDR"
+echo "Data: $SCRIPT_ADDR = "\'[\"${SCRIPT_ADDR:0:64}\",\"${SCRIPT_ADDR:64}\"]\'
 echo "Datum hash: $DATUM_HASH"
 echo ""
-
+echo ""
 # Get list of all swap utxos
-echo "Pending Swaps on Script Address"
+echo "Pending Swaps on Script Address:"
+printf '%.s─' $(seq 1 $(tput cols))
 . $ROOT_DIR/balance-addr.sh $SCRIPT_ADDR | grep Swap
 echo ""
-echo "Liquidity Pool on Script Address (has script address of datum hash)"
+echo ""
+echo "Liquidity Pool on Script Address (has script address of datum hash):"
+printf '%.s─' $(seq 1 $(tput cols))
 . $ROOT_DIR/balance-addr.sh $SCRIPT_ADDR | grep $DATUM_HASH
 echo ""
 
--- use-cases/swap/utils/batch.sh
+++ use-cases/swap/utils/batch.sh
@@ -1,24 +1,32 @@
 echo "Building Tx ..."
 set -xe
 
-# Dodati zamenske vredonosti valute na tx-out i takodje LP utxo n aulazu, za collateral staviti isto utxo novcanika koji izvrsava
+# Dodati zamenske vredonosti valute na tx-out i takodje LP utxo na ulazu, za collateral staviti isto utxo novcanika koji izvrsava
 ${CARDANO_CLI_PATH} transaction build \
   --alonzo-era \
   $NETWORK \
+  \
   --tx-in 1f229b50cbd6340a3dec937929ea6c2332c8e632e56ea1b131b6a3c19a7d9b84#1 \
   --tx-in-script-file $SCRIPT \
   --tx-in-datum-value '["addr_test1qqp6z9xneafqp0ndrxgyjd3qecs4tf6ktqhvj2hyc5efg2gzq5lqcm","6rfuv6yp0mtsgc2qxvwlwr938qk2nue0rqg6rqxewe52"]' \
   --tx-in-redeemer-value 1 \
-  --tx-out addr_test1qqp6z9xneafqp0ndrxgyjd3qecs4tf6ktqhvj2hyc5efg2gzq5lqcm6rfuv6yp0mtsgc2qxvwlwr938qk2nue0rqg6rqxewe52+2000345+"100 d9a1156d008866951090923bb1d39587aaebb342c50e5fb848f5d84f.SwapToken" \
+  --tx-out addr_test1qqp6z9xneafqp0ndrxgyjd3qecs4tf6ktqhvj2hyc5efg2gzq5lqcm6rfuv6yp0mtsgc2qxvwlwr938qk2nue0rqg6rqxewe52+2000345+"100 d9a1156d008866951090923bb1d39587aaebb342c50e5fb848f5d84f.SwapToken"+"1 29856eae5151337853ffbd8fd80df78dab8c7f09c37c95799328abb8.REVU" \
   \
   --tx-in fe809da736d315b8b3c99d39e3c74a0e053d8899943c92915cfe324db7a41472#1 \
   --tx-in-script-file $SCRIPT \
   --tx-in-datum-value '["addr_test1qqvxdvjcrwzqskge258qwax59nmuvt0veflk8pn4mmx603zv5t38e7","9gj3lgmvfdm8ntr2yau5khprzyyhzvn3lx04ssxrx6mt"]' \
   --tx-in-redeemer-value 100 \
-  --tx-out addr_test1qqvxdvjcrwzqskge258qwax59nmuvt0veflk8pn4mmx603zv5t38e79gj3lgmvfdm8ntr2yau5khprzyyhzvn3lx04ssxrx6mt+2000123+"205 d9a1156d008866951090923bb1d39587aaebb342c50e5fb848f5d84f.SwapToken"
+  --tx-out addr_test1qqvxdvjcrwzqskge258qwax59nmuvt0veflk8pn4mmx603zv5t38e79gj3lgmvfdm8ntr2yau5khprzyyhzvn3lx04ssxrx6mt+2000123+"205 d9a1156d008866951090923bb1d39587aaebb342c50e5fb848f5d84f.SwapToken"+"1 29856eae5151337853ffbd8fd80df78dab8c7f09c37c95799328abb8.REVU" \
+  \
+  --tx-in 5e2aa10d8ca9007d04d884fb248c9a3ae798dbc3ccc66b790f6b25e601b31a82#1 \
+  --tx-in-script-file $SCRIPT \
+  --tx-in-datum-value '["addr_test1wpnlxv2xv9a9ucvnvzqakwepzl9ltx7jzgm53av2e9ncv4sysemm8",""]' \
+  --tx-in-redeemer-value 100 \
+  --tx-out $SCRIPT_ADDR+4794457+"8 29856eae5151337853ffbd8fd80df78dab8c7f09c37c95799328abb8.REVU" \
+  --tx-out-datum-hash $DATUM_HASH \
   \
   --tx-in-collateral $MY_UTXO \
-  --protocol-params-file protocol.json \
+  --protocol-params-file $ROOT_DIR/protocol.json \
   --change-address $MY_ADDR \
   --out-file tx.build
 set +x
